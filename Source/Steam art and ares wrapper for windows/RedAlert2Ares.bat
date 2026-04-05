843518a9a83fd02b5a7350f4260ac9e928d57167
KOYK <eloyrian@gmail.com>
2026-04-05 18:57:12 +0300

    file stracture and phobos.dll

--- Source/Steam art and ares wrapper for windows/RedAlert2Ares.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set GAME_DIR=G:\Games\Steam\SteamLibrary\steamapps\common\Command ^& Conquer Red Alert II
-
-cd /d "%GAME_DIR%"
-
-start "" "%GAME_DIR%\Syringe.exe" "%GAME_DIR%\gamemd.exe" %*
